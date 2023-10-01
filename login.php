2f35f588074a00749de5a713422489e894943765
JenniferKamily <jenniferparreira2017@gmail.com>
2023-10-01 18:13:58 -0300

    1.3

--- login.php
+++ login.php
@@ -8,17 +8,25 @@ session_start();
 
     $nome = mysqli_real_escape_string($conexao,$_POST['nome']);
     $senha = mysqli_real_escape_string($conexao, $_POST['senha']);
+    $tipo = mysqli_real_escape_string($conexao, $_POST['tipo']);
 
-    $query= "select id, tipo from cadastro where nome = '{$nome}' and senha = md5('{$senha}')";
+    $query= "select*from cadastro where nome = '{$nome}' and senha = md5('{$senha}' and tipo = '{$tipo}')";
 
     $result = mysqli_query($conexao,$query);
     $row = mysqli_num_rows($result);
     $_SESSION['senha'] = $senha;
 
-    if ($row == 1){
+    if ($row == 1 && $tipo = 'adm'){
         $_SESSION['nome'] = $nome;
-
-        header('Location: login_aceito.php');
+        header('Location: adm.html');
+        exit();
+    }else if($row == 1 && $tipo = 'voluntario'){
+        $_SESSION['nome'] = $nome;
+        header('Location: voluntario.html');
+        exit();
+    }else if($row == 1 && $tipo = 'necessitado'){
+        $_SESSION['nome'] = $nome;
+        header('Location: necessitado.html');
         exit();
     }else {
         header('Location: login.php');
